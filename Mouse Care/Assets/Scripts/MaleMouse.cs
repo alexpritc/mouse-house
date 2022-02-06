ef71dcb8eb2481203e3e4f6792bea11a19fa5d34
Alex Pritchard <hi@apritc.com>
2022-02-06 10:44:18 +0000

    Added sex variants

---
+++ Mouse Care/Assets/Scripts/MaleMouse.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleMouse : Mouse
+{
+    /// <summary>
+    /// Measures how attractive the mouse is to potential mates
+    /// </summary>
+    private float _desirabilityByOtherSex;
+    
+    /// <summary>
+    /// How aggressive the mouse is to potential rivals
+    /// </summary>
+    private float _territorial;
+}
--- Mouse Care/Assets/Scripts/Mouse.cs
+++ Mouse Care/Assets/Scripts/Mouse.cs
@@ -6,56 +6,85 @@ using UnityEngine.AI;
 
 public class Mouse : MonoBehaviour {
     private string _status;
-    
+
     private int _id;
     private string _name;
     private char _sex;
-    
+    private string _breed;
+    private string _temprement;
+
     /// <summary>
     /// Age of mouse in months.
     /// </summary>
-    private string _breed;
-    private string _temprement;
-    
-    /// <summary>
-    /// Measures how attractive the mouse is to potential mates
-    /// </summary>
-    private float _desirabilityByOtherSex;
-    private float _territorial;
-    
     private int _age;
+
     private int _weight;
     private bool _isFertile;
-    private bool _isPregnant;
 
     private float _hunger;
     private float _thirst;
+
     /// <summary>
     /// How loneley is the mouse
     /// </summary>
     private float _socialisation;
+
     private float _reproductiveUrge;
     private float _stress;
-    
+
+    /// <summary>
+    /// How far the mouse can see around them
+    /// </summary>
+    [SerializeField] private float _sensoryRadius;
+    [SerializeField] private float _speed;
+
     public GameObject target;
 
     private NavMeshAgent _navMeshAgent;
 
     private void Start() {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.speed = _speed;
+
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.SetVertexCount(segments + 1);
+        line.useWorldSpace = false;
+        CreatePoints();
     }
 
     // Update is called once per frame
-    void Update () {
-        
+    void Update() {
+
         SetDestination(target.transform.position);
     }
 
-    private void SetDestination(Vector3 targetPos) {
+    public void SetDestination(Vector3 targetPos) {
         _navMeshAgent.destination = targetPos;
     }
-    
-    private void SetDestination(Transform target) {
+
+    public void SetDestination(Transform target) {
         _navMeshAgent.destination = target.position;
+    }
+
+
+    [Range(0, 100)] public int segments = 100;
+
+    private LineRenderer line;
+
+    void CreatePoints() {
+        float x;
+        float z;
+
+        float angle = 20f;
+
+        for (int i = 0; i < (segments + 1); i++) {
+            x = Mathf.Cos(Mathf.Deg2Rad * angle) * _sensoryRadius;
+            z = Mathf.Sin(Mathf.Deg2Rad * angle) * _sensoryRadius;
+
+            line.SetPosition(i, new Vector3(x, 0, z));
+
+            angle += (360f / segments);
+        }
     }
 }
