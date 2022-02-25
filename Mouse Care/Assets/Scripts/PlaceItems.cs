ecdb0dc0808ec84f1883d6247ae3251a2b787a79
Alex Pritchard <hi@apritc.com>
2022-02-25 19:11:07 +0000

    Added previews to placing items

--- Mouse Care/Assets/Scripts/PlaceItems.cs
+++ Mouse Care/Assets/Scripts/PlaceItems.cs
@@ -10,29 +10,65 @@ public class PlaceItems : MonoBehaviour
     private Controls _controls;
 
     [SerializeField] private Camera mainCamera;
+
+    [SerializeField] private bool isItemSelected;
+
+
+    private GameObject preview;
+    [SerializeField] private Material previewMat;
+    [SerializeField] private Material previewMatRed;
+
+    [SerializeField] private LayerMask meshLayer;
+    
     private void Awake() {
         _controls = new Controls();
         
         _controls.Item.PlaceItem.performed += ctx => PlaceItem();
+        preview = null;
     }
 
     private void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        { 
-            if (hit.collider.gameObject.tag == "Mesh")
-            {
-                Debug.Log(true);
-            }
-            else
-            {
-                Debug.Log(false);
+        if (isItemSelected)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            { 
+                if (preview == null)
+                {
+                    preview = Instantiate(cubePrefab);
+                    preview.GetComponent<Collider>().enabled = false;
+                    preview.GetComponent<MeshRenderer>().material = previewMat;
+                }
+                
+                if (hit.collider.gameObject.tag == "Mesh")
+                {
+                    preview.GetComponent<MeshRenderer>().material = previewMat;
+                    
+                    preview.transform.rotation = new Quaternion(hit.normal.x, hit.normal.y, hit.normal.z,
+                        cubePrefab.transform.rotation.w);
+                }
+                else
+                {
+                    preview.GetComponent<MeshRenderer>().material = previewMatRed;
+
+                    Ray ray2 = new Ray(hit.point, hit.point - Camera.main.transform.position);
+                    
+                    if (Physics.Raycast(ray2, out RaycastHit hit2, Mathf.Infinity, meshLayer.value))
+                    {
+                        preview.transform.rotation = new Quaternion(hit2.normal.x, hit2.normal.y, hit2.normal.z,
+                            cubePrefab.transform.rotation.w);
+                    }
+                }
+                
+                preview.transform.position =
+                    hit.point + new Vector3(0f, preview.transform.localScale.y / 2, 0f);
+
             }
         }
     }
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmosSelected() {
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
@@ -59,6 +95,9 @@ public class PlaceItems : MonoBehaviour
                 GameObject go = Instantiate(cubePrefab, hit.point + new Vector3(0f,cubePrefab.transform.localScale.y / 2,0f),
                     new Quaternion(hit.normal.x, hit.normal.y, hit.normal.z, cubePrefab.transform.rotation.w));
             }
+            
+            Destroy(preview);
+            preview = null;
         }
     }
 
