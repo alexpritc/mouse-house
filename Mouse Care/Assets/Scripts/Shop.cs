7bc5fa305c995b7d9dd9a096d2aa646ef40528d3
Alex Pritchard <hi@apritc.com>
2022-03-03 19:02:50 +0000

    Added UI/Shop panel with basic animations

---
+++ Mouse Care/Assets/Scripts/Shop.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private Animator panelAnimator;
+    string clipName;
+    AnimatorClipInfo[] currentClipInfo;
+
+    private GameObject currentOpenShop;
+
+    [SerializeField] private Color buttonSelected;
+    [SerializeField] private Color buttonNormal;
+
+    public void PlayPanelAnim(GameObject shopButton)
+    {
+        //Access the Animation clip name
+        clipName = panelAnimator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        
+        // If its closed or closing
+        if (clipName == "PanelIdleClosed" || clipName == "PanelClosed")
+        {
+            if (currentOpenShop != null)
+            {
+                currentOpenShop.GetComponent<Image>().color = buttonNormal;
+            }
+            
+            shopButton.GetComponent<Image>().color = buttonSelected;
+            panelAnimator.CrossFade("Opening", 0.2f);
+            currentOpenShop = shopButton;
+        }
+
+        // If its open or opening
+        else if (clipName == "PanelOpen" || clipName == "PanelIdleOpen")
+        {
+            // Only close if pressing the same button twice
+            if (currentOpenShop == shopButton)
+            {
+                panelAnimator.CrossFade("Closing", 0.2f);
+                shopButton.GetComponent<Image>().color = buttonNormal;
+            }
+            else
+            {
+                // No animations, just change contents
+                if (currentOpenShop != null)
+                {
+                    currentOpenShop.GetComponent<Image>().color = buttonNormal;
+                }
+                shopButton.GetComponent<Image>().color = buttonSelected;
+                currentOpenShop = shopButton;
+            }
+        }
+    }
+}
