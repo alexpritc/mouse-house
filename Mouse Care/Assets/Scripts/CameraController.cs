23dbee7f9f39aa2cbd82d7f2763ed25bd64b89c6
Alex Pritchard <hi@apritc.com>
2022-03-07 16:27:05 +0000

    Updated camera rotation

--- Mouse Care/Assets/Scripts/CameraController.cs
+++ Mouse Care/Assets/Scripts/CameraController.cs
@@ -8,8 +8,6 @@ using static UnityEngine.InputSystem.Mouse;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CameraController : MonoBehaviour {
-    private Vector2 touchPos;
-
     [SerializeField] private float _horizontalRotationSpeed = 2f;
     [SerializeField] private float _verticalRotationSpeed = 1f;
     [SerializeField] private float _panningSpeed = 0.75f;
@@ -19,16 +17,11 @@ public class CameraController : MonoBehaviour {
     
     private bool _isRotating;
     private bool _isPanning;
-
-    private Vector3 _touchStart;
+    
     private Vector3 _direction;
 
     [SerializeField] private GameObject _cameraPivot;
     [SerializeField] private GameObject _camera;
-
-    private Vector2 turn;
-
-    private float _rot;
 
     private bool _isZooming;
     private float _zoomModifer;
@@ -36,15 +29,16 @@ public class CameraController : MonoBehaviour {
     private float y;
 
     private Vector2 moveInput;
-    
-    private float _verticalThreshold = 0.2f;
-    private float _horizontalThreshold = 0.1f;
+
+    private Vector2 mousePosThisFrame;
+    private Vector2 mousePosLastFrame;
+
+    private float pitch;
+    private float yaw;
 
     private void Awake() {
         controls = new Controls();
-        
-        controls.Camera.GetMouseStartPos.performed += ctx => _touchStart = cursorWorldPosOnNCP;
-        
+
         controls.Camera.PanCamera.performed += ctx => _isPanning = true;
         controls.Camera.PanCamera.canceled += ctx => _isPanning = false;
         
@@ -62,78 +56,38 @@ public class CameraController : MonoBehaviour {
         controls.Camera.Down.performed += ctx => y = -1f;
         controls.Camera.Up.canceled += ctx => y = 0f;
         controls.Camera.Down.canceled += ctx => y = 0f;
+        
+        controls.Camera.MousePosition.performed += ctx => mousePosThisFrame = ctx.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        _direction = mousePosLastFrame - mousePosThisFrame;
+        
+        Vector3 move = new Vector3(moveInput.x, y, moveInput.y);
+        _cameraPivot.transform.Translate(move * _movementSpeed, Space.Self);
+
         if (_isZooming)
         {
             _zoomModifer = Mathf.Clamp(_zoomModifer, -5f, 5f);
             _camera.transform.position += _camera.transform.forward * _zoomModifer;
         }
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 move = new Vector3(moveInput.x, y, moveInput.y);
-        _cameraPivot.transform.Translate(move * _movementSpeed, Space.Self);
-        
-        if (_isPanning && !GameManager.Instance.IsInPlaceItemMode)
+        else if (_isPanning && !GameManager.Instance.IsInPlaceItemMode)
         {
-            _direction = _touchStart - cursorWorldPosOnNCP;
+            // _direction = _touchStart - cursorWorldPosOnNCP;
             _cameraPivot.transform.Translate(_direction * _panningSpeed, Space.Self);
         }
-        else if (_isRotating) {
-            _direction = _touchStart - cursorWorldPosOnNCP;
+        else if (_isRotating)
+        {
+            pitch += _direction.y * _verticalRotationSpeed;
+            pitch = Mathf.Clamp(pitch, -20f, 70.0f);
+            yaw += _direction.x * _horizontalRotationSpeed;
 
-            // Standardises rotation speed
-            _direction = StandardiseRotationSpeed(_direction);
-
-            // Needs to be way more sensitive for horizontal rotation than vertical
-            float pitch = _cameraPivot.transform.eulerAngles.x + (_direction.y * _verticalRotationSpeed);
-            float yaw = transform.eulerAngles.y + (_direction.x * _horizontalRotationSpeed);
-            
-            // clamp pitch
-            pitch = Mathf.Clamp(pitch, 0f, 70f);
-
-            Quaternion rot;
-            
-            if (Mathf.Abs(_direction.x) > _horizontalThreshold && Mathf.Abs(_direction.y) > _verticalThreshold)
-            {
-                rot = Quaternion.Euler(pitch, yaw, 0 );
-            }
-            else if (Mathf.Abs(_direction.x) > _horizontalThreshold)
-            {
-                rot = Quaternion.Euler(0, yaw, 0 );
-            }
-            else
-            {
-                rot = Quaternion.Euler(pitch, 0, 0 );
-            }
-
-            transform.rotation = rot;
+            transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
         }
-    }
 
-    private Vector3 StandardiseRotationSpeed(Vector3 inVector)
-    {
-        float x = 0f, y = 0f, z = 0f;
-        
-        x = inVector.x < 0 ? -1 : 1;
-        y = inVector.y < 0 ? -1 : 1;
-        z = inVector.z < 0 ? -1 : 1;
-
-        return new Vector3(x, y, z);
-    }
-
-    private static Vector3 cursorWorldPosOnNCP {
-        get {
-            return Camera.main.ScreenToViewportPoint(
-                new Vector3(Input.mousePosition.x, 
-                    Input.mousePosition.y, 
-                    Camera.main.nearClipPlane));
-        }
+        mousePosLastFrame = mousePosThisFrame;
     }
 
     private void OnEnable() {
